e35d69dbcf798c4bdb053ab85e89dd4ff24dc6a6
ThunuguntlaRohini <44874103+ThunuguntlaRohini@users.noreply.github.com>
2018-11-09 16:58:23 +0530

    Add files via upload

---
+++ 04_JDBCannotationtest/src/com/training/client/Test4.java
@@ -0,0 +1,18 @@
+package com.training.client;
+
+/**
+ * 
+ * @author rthunugu
+ *
+ */
+public class Test4 {
+	/**
+	 * 
+	 * @param n: Number to be computed for square
+	 * @return return square of the number
+	 */
+	int square(int n){
+		return n*n;
+	} 
+
+}
